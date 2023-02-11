273f8c195c165fed6b431a8645b00f0c5b97e984
vinitver <124779474+vinitver@users.noreply.github.com>
2023-02-11 23:11:02 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function imgchange(){
+    document.querySelector(".img").classList.remove("imgmon");
+    document.querySelector(".img").classList.add("imgeye");
+    
+}
+function unchange(){
+    
+    document.querySelector(".img").classList.remove("imgeye");
+    document.querySelector(".img").classList.add("imgmon");
+}
+ 
